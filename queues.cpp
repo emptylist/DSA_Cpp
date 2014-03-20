f140b5bdbc17eee88f5e408a0a722cd1de0a69cd
emptylist <crooks1379@gmail.com>
2014-03-20 16:07:34 -0500

    queues re-sub (wrong git add location)

--- queues.cpp
+++ queues.cpp
@@ -12,6 +12,11 @@ public:
   string getMessage() const {return errorMsg; }
 };
 
+class QueueFullException : public RuntimeException {
+public:
+  QueueFullException(const string& err) : RuntimeException(err) {}
+};
+
 // Queue Template
 
 template <typename Object>
@@ -23,10 +28,44 @@ private:
   int front;
   int rear;
 public:
+  //Constructor, Destructor, etc.
+  ArrayQueue(int cap = CAPACITY) {
+    capacity = cap;
+    Q = new Object[capacity];
+    f = r = 0;
+  }
+
+  ~ArrayQueue() {
+    delete [] Q;
+  }
+
+  ArrayQueue& operator=(const ArrayQueue qu); // assignment operator constructor
+  ArrayQueue(const ArrayQueue& qu); //copy constructor
+
+  // Data Structure Logic methods
+  void enqueue(const Object& obj) throw(QueueFullException) {
+    if (size() == capacity)
+      throw QueueFullException("Queue overflow.");
+    Q[r] = obj;
+    r = r++ % capacity;
+  }
+
+  Object dequeue() throw(QueueEmptyException) {
+    if (size() == 0)
+      throw QueueEmptyException("Access to empty Queue.");
+    output_loc = f;
+    f = f++ % capacity;
+    return Q[output_loc];
+  }
+
+  // Helper methods
   int size() const {
     return (capacity - front + rear) % capacity;
   }
+
   bool isEmpty const {
     return (front == rear);
   }
-  
+  const Object& first() const throw(QueueEmptyException) {
+  }
+};
